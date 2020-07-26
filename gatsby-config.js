2eacd8f7b741476aadbe0c39a7319d94a1ec84b3
dotcorps <wervank@gmail.com>
2020-07-26 15:00:19 +0530

    config file backstick removed for localhost

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     {
     resolve: `gatsby-source-strapi`,
      options: {
-      apiURL:process.env.API_URL||`http://localhost:1337`,
+      apiURL:process.env.API_URL||'http://localhost:1337',
       queryLimit: 200, // Default to 100
       contentTypes: [`product`] 
     },
